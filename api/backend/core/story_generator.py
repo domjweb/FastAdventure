b6199f1aa56db9e4a6aeaa62c271c75d4984749d
domjweb <dominiquejadonwebb@gmail.com>
2025-10-13 16:59:09 -0400

    More API changes

--- api/backend/core/story_generator.py
+++ api/backend/core/story_generator.py
@@ -2,9 +2,9 @@ from sqlalchemy.orm import Session
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-from core.prompts import STORY_PROMPT
-from models.story import Story, StoryNode
-from core.models import StoryLLMResponse, StoryNodeLLM
+from backend.core.prompts import STORY_PROMPT
+from backend.models.story import Story, StoryNode
+from backend.core.models import StoryLLMResponse, StoryNodeLLM
 from dotenv import load_dotenv
 
 load_dotenv()
--- api/backend/routers/job.py
+++ api/backend/routers/job.py
@@ -3,8 +3,8 @@ from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, Cookie
 from sqlalchemy.orm import Session
 from db.database import get_db
-from models.job import StoryJob
-from schemas.job import StoryJobResponse
+from backend.models.job import StoryJob
+from backend.schemas.job import StoryJobResponse
 
 router = APIRouter (
     prefix="/jobs",
