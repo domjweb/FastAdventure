777bfc7f51847762c622831280386be72a263f11
domjweb <dominiquejadonwebb@gmail.com>
2025-10-14 11:42:55 -0400

    Rearranging repo for Azure API

--- backend/function_app.py
+++ backend/function_app.py
@@ -5,5 +5,3 @@ from main import app  # Import your FastAPI app
 handler = Mangum(app)
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-	return func.AsgiMiddleware(handler).handle(req, context)
-	return func.AsgiMiddleware(handler).handle(req, context)
