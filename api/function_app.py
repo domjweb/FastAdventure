33e7b38ed9b09105ee627b6bc94fb706fe93a5e4
domjweb <dominiquejadonwebb@gmail.com>
2025-10-14 12:09:17 -0400

    Re-added mangum import

--- api/function_app.py
+++ api/function_app.py
@@ -1,9 +1,4 @@
-import azure.functions as func
 from mangum import Mangum
-from main import app  # Import your FastAPI app
+from main import app
 
 handler = Mangum(app)
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-	return func.AsgiMiddleware(handler).handle(req, context)
-	return func.AsgiMiddleware(handler).handle(req, context)
